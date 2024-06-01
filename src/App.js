f149ffa296e4eab3fa12ce7d1ee3f8d411553c10
balakrishnan4591 <balakrishnan4591@gmail.com>
2024-06-01 12:25:13 +0530

    fist-test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Child from "./components/child/Child";
 
-function App() {
+const App = () => {
+  // const type = "text";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Child name="Bala" age="33" />
+      <Child name="Krish" age="24" />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/child/Child.js
@@ -0,0 +1,8 @@
+function Child(props) {
+  return (
+    <header>
+      My Name is {props.name} and my age is {props.age}
+    </header>
+  );
+}
+export default Child;
